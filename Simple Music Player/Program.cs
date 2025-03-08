51207946fea8d3ec36a54a7d7e161745c50196ea
Richie <yourfriend2428@gmail.com>
2025-03-08 17:37:06 +0800

    First commit

---
+++ Simple Music Player/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Music_Player
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PlayMusic("moon.wav");
+            Console.WriteLine("Music is Playing!");
+            Console.ReadLine();
+        }
+
+        public static void PlayMusic(string filepath)
+        {
+            SoundPlayer musicPlayer = new SoundPlayer();
+            musicPlayer.SoundLocation = filepath;
+            musicPlayer.Play();
+        }
+    }
+}
